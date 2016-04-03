61cda95499f484477f0b3d7beb11bb00a50ede8b
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 11:04:32 +1200

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
+//var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
+var query = "https://api.wheretheiss.at/v1/satellites/25544"
+
+xhr.get('/v1/cats', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  document.body.innerHTML = example({ name: "fluffy" });
 })
-
