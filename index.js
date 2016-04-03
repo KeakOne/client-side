cda979eb6a9e2058d1e405839db7691bbba0e56e
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 11:37:54 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var example = require('./views/homeview.hbs')
 var app = require('express');
-
+var cats = require("./json-cats/app.js")
 
 var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
 //var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
@@ -14,7 +14,7 @@ xhr.get(query, function(err, data) {
   document.body.innerHTML = example({ name: "fluffy" });
 })
 
-app.use('http://localhost:3000/v1/cats', cats);
+app.use('/v1/cats', cats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
