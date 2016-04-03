179932688d82564e6aec3d205eef946862171fbe
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 10:01:54 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
+var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
+
+xhr.get(query, function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
