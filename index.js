f41bd1363ece5d0e952e8525921e10e6811e8e71
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 15:31:56 +1200

    Updates

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
 //var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
 // var query = "https://api.wheretheiss.at/v1/satellites/25544"
 $(document).ready(function(){
+  console.log("hi");
   $("#submit").click(function(){
      xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
         if (err) console.log(err) // do something
