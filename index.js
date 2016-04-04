1893e41fc37a4f176185608f6e7f43653530d576
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 16:10:45 +1200

    add button, refactor and use jquery

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var Satellite = require('./views/Satellite.hbs')
+var $ = require("jquery")
 
+//var button =document.getElementsByTagName("a");
 var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
 //var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
-var query = "https://api.wheretheiss.at/v1/satellites/25544"
+// var query = "https://api.wheretheiss.at/v1/satellites/25544"
+$(document).ready(function(){
+  $("#thebutton").click(function(){
+     xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+        if (err) console.log(err) // do something
+        var body = JSON.parse(data.body)
+        var satelliteInfo = { name: "Satellite " + body.name, lat: body.latitude, lon: body.longitude, }
+        console.log(body)
+        document.getElementById("results").innerHTML = Satellite(satelliteInfo);
+      })
+  })
 
-xhr.get('/v1/cats', function(err, data) {
-  if (err) console.log(err) // do something
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "fluffy" });
 })
+
