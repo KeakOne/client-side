732ae81902e1804ca004320c2977c3c2140233e3
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 13:38:05 +1200

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var Satellite= require('./views/Satellite.hbs')
 
 var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
 //var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
@@ -10,5 +10,5 @@ xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
     var body = JSON.parse(data.body)
 
   console.log(body)
-  document.body.innerHTML = example({ name: "Sattelite " + body.name, lat: body.latitude, lon: body.longitude, });
+  document.body.innerHTML = Satellite({ name: "Satellite " + body.name, lat: body.latitude, lon: body.longitude, });
 })
