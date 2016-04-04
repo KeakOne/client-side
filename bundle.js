5aa08ebd0147d61a7d4b6ff5a791bba1283c0caf
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 13:00:22 +1200

    Updates

--- bundle.js
+++ bundle.js
@@ -89,6 +89,6 @@ function once(n){var o=!1;return function(){return o?void 0:(o=!0,n.apply(this,a
 function extend(){for(var r={},e=0;e<arguments.length;e++){var t=arguments[e];for(var n in t)hasOwnProperty.call(t,n)&&(r[n]=t[n])}return r}module.exports=extend;var hasOwnProperty=Object.prototype.hasOwnProperty;
 
 },{}],30:[function(require,module,exports){
-var HandlebarsCompiler=require("hbsfy/runtime");module.exports=HandlebarsCompiler.template({compiler:[7,">= 4.0.0"],main:function(e,a,n,l,r){var i;return"<h1>Hello "+e.escapeExpression((i=null!=(i=n.name||(null!=a?a.name:a))?i:n.helperMissing,"function"==typeof i?i.call(null!=a?a:{},{name:"name",hash:{},data:r}):i))+"!</h1>\n"},useData:!0});
+var HandlebarsCompiler=require("hbsfy/runtime");module.exports=HandlebarsCompiler.template({compiler:[7,">= 4.0.0"],main:function(l,a,n,e,t){var u,i=null!=a?a:{},o=n.helperMissing,r="function",m=l.escapeExpression;return"<h1>Hello "+m((u=null!=(u=n.name||(null!=a?a.name:a))?u:o,typeof u===r?u.call(i,{name:"name",hash:{},data:t}):u))+"!</h1>\n<ul>\n  <li>"+m((u=null!=(u=n.lat||(null!=a?a.lat:a))?u:o,typeof u===r?u.call(i,{name:"lat",hash:{},data:t}):u))+"</li>\n  <li>"+m((u=null!=(u=n.lon||(null!=a?a.lon:a))?u:o,typeof u===r?u.call(i,{name:"lon",hash:{},data:t}):u))+"</li>\n</ul>\n"},useData:!0});
 
 },{"hbsfy/runtime":23}]},{},[1]);
