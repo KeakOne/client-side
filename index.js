b39c624682900e12bcc19d78f55ee51bb76cffc8
James Sangalli & K. Carnevale <pair+james-sangalli.keakone@devbootcamp.com>
2016-04-04 12:46:56 +1200

    Updates

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var example = require('./views/example.hbs')
 
 var tx = "b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da";
 //var query = "https://blockchain.info/rawtx/"+tx+"/$tx_hash";
-var query = "https://api.wheretheiss.at/v1/satellites/25544"
+// var query = "https://api.wheretheiss.at/v1/satellites/25544"
 
-xhr.get(query, function(err, data) {
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
+    var body = JSON.parse
 
-  console.log(JSON.parse(data.body))
+  console.log(data.body)
   document.body.innerHTML = example({ name: "fluffy" });
 })
